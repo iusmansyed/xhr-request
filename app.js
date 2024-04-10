7101fc3fce90e146f2aa0219ca8c4291c7c557d1
Syed Muhammad Usman <101274577+iusmansyed@users.noreply.github.com>
2024-04-11 02:10:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-// var getBtn = document.getElementById('btn_1');
-// var postBtn = document.getElementById('btn_2');
-// const sentHttpRequest = (method, url, data) => {
-//     const promise = new Promise((resolve, reject) => {
-//         const xhr = new XMLHttpRequest();
-//         xhr.open(method, url);
-//         xhr.responseType = 'json'
-//         if (data) {
-//             xhr.setRequestHeader('Content-Type', 'application/json')
-//         }
-//         xhr.onload = () => {
-//             if (xhr.status >= 400) {
-//                 reject(xhr.response)
-//             } else {
-//                 resolve(xhr.response)
-//             }
-//         }
-//         xhr.onerror = () => {
-//             reject('something went wrong')
-//         }
-//         xhr.send(JSON.stringify(data))
-//     });
-//     return promise
-// }
+ var getBtn = document.getElementById('btn_1');
+ var postBtn = document.getElementById('btn_2');
+ const sentHttpRequest = (method, url, data) =>
+ {     const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json'
+        if (data) {
+            xhr.setRequestHeader('Content-Type', 'application/json')
+       }
+       xhr.onload = () => {
+             if (xhr.status >= 400) {
+               reject(xhr.response)
+            } else {
+                resolve(xhr.response)
+            }
+        }
+         xhr.onerror = () => {
+             reject('something went wrong')
+       }
+        xhr.send(JSON.stringify(data))
+   });
+   return promise
+ }
 
-// const getData = () => {
-//     sentHttpRequest('GET', 'https://reqres.in/api/users').then(res => {
-//         var list = document.getElementById('list');
+ const getData = () => {
+     sentHttpRequest('GET', 'https://reqres.in/api/users').then(res => {
+        var list = document.getElementById('list');
 //         list.innerHTML = '';
 //         res?.data?.forEach(function(user) {
 //             var userList = document.createElement('li');
